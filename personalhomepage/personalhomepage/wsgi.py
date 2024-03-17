c8aa98c877dedd6853c467d97c5eaa5fd429fbb6
pivog <planinic.ivki@gmail.com>
2024-03-17 11:49:23 +0000

    second commit

---
+++ personalhomepage/personalhomepage/wsgi.py
@@ -0,0 +1,25 @@
+"""
+WSGI config for personalhomepage project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+import sys
+
+
+sys.path.append('/home/ubuntu/djangoreact-personal-page/personalhomepage')
+sys.path.append('/home/ubuntu/djangoreact-personal-page/personalhomepage/personalhomepage')
+
+
+from django.core.wsgi import get_wsgi_application
+
+
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'personalhomepage.settings')
+
+application = get_wsgi_application()

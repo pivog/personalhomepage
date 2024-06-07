1175bcee89a0a6fb92503dfc19280c1d766f66a6
ivo <planinic.ivki@gmail.com>
2024-06-08 01:39:20 +0200

    forgot a file oops

--- personalhomepage/backend/models.py
+++ personalhomepage/backend/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Post(models.Model):
-    id = models.IntegerField(primary_key=True)
+    id = models.IntegerField(primary_key=True, unique=True)
     title = models.CharField(max_length=255)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
@@ -14,10 +14,21 @@ class Post(models.Model):
         return str(self.title) + " | " + str(self.author)
 
 
+class Project(models.Model):
+    id = models.IntegerField(primary_key=True, unique=True)
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    urlToSite = models.CharField(max_length=2040)
+    imageUrl = models.CharField(max_length=2040)
+
+    def __str__(self):
+        return str(self.title) + " | " + str(self.author)
+
+
 class ChessGame(models.Model):
-    id = models.IntegerField(primary_key=True)
+    id = models.IntegerField(primary_key=True, unique=True)
     pgn = models.TextField()
 
     def __str__(self):
         return "Game " + str(self.id)
-

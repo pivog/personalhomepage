b249afbfb1b5b58d1fa8cedd0da41749e011055d
Ivo <planinic.ivki@gmail.com>
2024-09-08 00:17:09 +0200

    adding back accidentally deleted files

---
+++ personalhomepage/frontend/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+
+from .views import index
+
+urlpatterns = [
+    # re_path(r".*", index),
+    path("", index),
+    path("about", index),
+    path("projects", index),
+    path("chessgames", index),
+    path("contact", index),
+    path("article", index),
+    path("latinski", index),
+    path("project", index),
+    path("listchessgames", index),
+    path("listpersonalchessgames", index),
+    path("login", index),
+]

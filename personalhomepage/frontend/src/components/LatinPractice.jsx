ab96ab1e33b0167993add3c6ff4d57ea474a0a37
root <root@vmi2559905.contaboserver.net>
2025-06-18 12:55:33 +0200

    from server latin

--- personalhomepage/frontend/src/components/LatinPractice.jsx
+++ personalhomepage/frontend/src/components/LatinPractice.jsx
@@ -9,7 +9,7 @@ import Typography from "@mui/material/Typography";
 
 
 const ButtonSelectionDialog = (props) => {
-    const available = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50"];
+    const available = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50"];
   const [selectedButtons, setSelectedButtons] = useState(available.map((value, _)=>{
       return [value, props.selected.includes(value)]
   }));
